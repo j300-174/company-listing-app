7cdb55e0b1df079b478da30ada33413461ea013d
j300-174 <jeddpicones@gmail.com>
2021-11-26 23:32:19 +0000

    deal amount (assuming valuation) command added

--- server/db/SQLScripts/tasks.sql
+++ server/db/SQLScripts/tasks.sql
@@ -19,11 +19,19 @@ SELECT tbl_companies.country, tbl_deals.amount_invested FROM tbl_deals, tbl_comp
 -- command retrieves all amount invested by each company & their country
 -- now need to workout avg per country
 SELECT tbl_deals.amount_invested, tbl_deals.company_name, tbl_companies.country
- FROM tbl_deals, tbl_companies WHERE tbl_deals.company_name = tbl_companies.name;
+FROM tbl_deals, tbl_companies WHERE tbl_deals.company_name = tbl_companies.name;
+-- deal amount (valuation)
+SELECT tbl_deals.valuation, tbl_deals.company_name, tbl_companies.country
+FROM tbl_deals, tbl_companies WHERE tbl_deals.company_name = tbl_companies.name;
 
 -- ** command works out the average invested grouped by country **
- SELECT AVG(tbl_deals.amount_invested), tbl_companies.country
-  FROM tbl_deals, tbl_companies WHERE tbl_deals.company_name = tbl_companies.name
-  GROUP BY tbl_companies.country;
+SELECT AVG(tbl_deals.amount_invested), tbl_companies.country
+FROM tbl_deals, tbl_companies WHERE tbl_deals.company_name = tbl_companies.name
+GROUP BY tbl_companies.country;
+
+-- valuation:
+SELECT AVG(tbl_deals.valuation), tbl_companies.country
+FROM tbl_deals, tbl_companies WHERE tbl_deals.company_name = tbl_companies.name
+GROUP BY tbl_companies.country;
 
 -- ** Total amount invested per quarter over time **
