337eb7c18ab2707e1f72de032ce26db9f73ff67f
j300-174 <jeddpicones@gmail.com>
2021-11-28 01:43:16 +0000

    setup routes for each page, one for company list, test, listings

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Listings from './routes/listings';
 import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/listings' element={<Listings />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
