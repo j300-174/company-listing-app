18ef54a7e6dddcf9706691eda1e4bb983285e166
j300-174 <jeddpicones@gmail.com>
2021-11-28 01:13:38 +0000

    cors setup for each request on specific urls, getCompany setup

--- server/index.js
+++ server/index.js
@@ -3,14 +3,26 @@ const app = express();
 const port = 3001;
 const { deal } = require('./dealsDatabase');
 const { company } = require('./companyDatabase');
+const cors = require('cors');
+
+// specific urls used for CORS
+app.use('/company/names', cors());
+app.use('/company/list', cors());
+
 app.get('/', (req, res) => {
   res.send('hello world');
 })
 
-app.get('/company/list', (req, res) => {
-  deal.getDeals();
-  company.getCompany();
-  res.send('check console if company listed');
+app.get('/company/list', async(req, res) => {
+  const list = await company.getCompany();
+  console.log('list of companies', list);
+  res.send(list);
+})
+
+app.get('/company/names', async(req, res) => {
+  const names = await company.getCompanyNames();
+  console.log('company names:', names);
+  res.send(names);
 })
 
 app.listen(port, () => {
