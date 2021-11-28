b8794d57f6cc98b4c3813729201cbeb23c5a9ad3
j300-174 <jeddpicones@gmail.com>
2021-11-28 01:39:45 +0000

    added list of companies on app, routes to listings company details

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,57 @@
+import { Link } from 'react-router-dom';
+import { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="">
-        <h1>Hello world</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    names: []
+  }
+
+  componentDidMount() {
+    const config = {
+      method: 'get',
+      url: 'http://localhost:3001/company/list'
+    }
+    axios.get(config.url)
+      .then(res => {
+        const companyNames = res.data;
+        this.setState({ names: companyNames });
+      });
+  }
+
+  render() {
+    let companyNames = this.state.names;
+    console.log(companyNames);
+    return (
+      <div className="App">
+        <Link to='/foo'>Foo</Link>
+        <Link to={{
+          pathname:'/listings'
+        }}>
+          Listings
+        </Link>
+        <header className="">
+          <h1>Company Listings Ltd.</h1>
+          <div>
+            <ul>
+            {companyNames.map((company, index) => (
+              <li key={index}>
+                <Link
+                  to={`/listings/${company.name}`}
+                  key={company.name}
+                >
+                  <h2>{company.name}</h2>
+                </Link>
+                <button type='submit'>Follow</button>
+              </li>
+            ))}
+            </ul>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
