d14199c8890ed85742009a01dfe1ec00ce6b3f87
j300-174 <jeddpicones@gmail.com>
2021-11-28 02:29:33 +0000

    Added answers for each task

--- server/db/SQLScripts/tasks.sql
+++ server/db/SQLScripts/tasks.sql
@@ -1,7 +1,11 @@
 -- ** Average employee count **
 -- ANS:
-SELECT AVG(employee_count) FROM tbl_companies; -- 235.10
-
+SELECT AVG(employee_count) FROM tbl_companies;
+/*
+  avg
+  ----------------------
+  235.1000000000000000
+*/
 -- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 -- Average deal amount raised by country
@@ -15,9 +19,13 @@ SELECT * FROM tbl_deals JOIN tbl_companies ON company_name = name;
 SELECT company_name, AVG(valuation) FROM tbl_deals GROUP BY company_name;
 SELECT company_name, AVG(amount_invested) FROM tbl_deals GROUP BY company_name;
 
--- retrieved all countries & valuation || amount invested, now needs to workout avg
-SELECT tbl_companies.country, tbl_deals.valuation FROM tbl_deals, tbl_companies GROUP BY tbl_deals.valuation, tbl_companies.country;
-SELECT tbl_companies.country, tbl_deals.amount_invested FROM tbl_deals, tbl_companies GROUP BY tbl_deals.amount_invested, tbl_companies.country;
+-- retrieved all countries & valuation ||
+-- amount invested, now needs to workout avg
+SELECT tbl_companies.country, tbl_deals.valuation FROM tbl_deals, tbl_companies
+GROUP BY tbl_deals.valuation, tbl_companies.country;
+
+SELECT tbl_companies.country, tbl_deals.amount_invested FROM tbl_deals,
+tbl_companies GROUP BY tbl_deals.amount_invested, tbl_companies.country;
 
 -- command retrieves all amount invested by each company & their country
 -- now need to workout avg per country
@@ -28,6 +36,13 @@ SELECT tbl_deals.valuation, tbl_deals.company_name, tbl_companies.country
 FROM tbl_deals, tbl_companies WHERE tbl_deals.company_name = tbl_companies.name;
 
 -- ** command works out the average invested grouped by country **
+/*
+  avg          | country
+  -----------------------+---------
+  13050000.000000000000 | GBR
+  550000.000000000000 | USA
+*/
+
 SELECT AVG(tbl_deals.amount_invested), tbl_companies.country
 FROM tbl_deals, tbl_companies WHERE tbl_deals.company_name = tbl_companies.name
 GROUP BY tbl_companies.country;
@@ -41,11 +56,25 @@ GROUP BY tbl_companies.country;
 
 -- ** Total amount invested per quarter over time **
 -- Sums up amount invested by quarter, need to consider yearly aswell
-SELECT SUM(tbl_deals.amount_invested), date_part('quarter', date) AS quarter FROM tbl_deals GROUP BY quarter;
+SELECT SUM(tbl_deals.amount_invested), date_part('quarter', date) AS quarter
+FROM tbl_deals GROUP BY quarter;
 -- sums amount invested by quarter for each year
-SELECT SUM(tbl_deals.amount_invested), date_part('quarter', date) AS quarter, date_part('year', date) AS year FROM tbl_deals GROUP BY quarter, year, amount_invested;
+SELECT SUM(tbl_deals.amount_invested), date_part('quarter', date) AS quarter,
+date_part('year', date) AS year FROM tbl_deals GROUP BY quarter, year,
+amount_invested;
 -- sums amount
-SELECT SUM(tbl_deals.amount_invested), date_part('quarter', date) AS quarter, date_part('year', date) AS year FROM tbl_deals GROUP BY quarter, year;
+SELECT SUM(tbl_deals.amount_invested), date_part('quarter', date) AS quarter,
+date_part('year', date) AS year FROM tbl_deals GROUP BY quarter, year;
 -- SUMS amount invested per quarter, per year
--- ANS: 
-SELECT SUM(amount_invested), date_part('quarter', date) AS quarter, date_part('year', date) AS year FROM tbl_deals GROUP BY year, quarter;
+-- ANS:
+SELECT SUM(amount_invested), date_part('quarter', date) AS quarter,
+date_part('year', date) AS year FROM tbl_deals GROUP BY year, quarter;
+/*
+  sum    | quarter | year
+  ----------+---------+------
+  10000000 |       1 | 2021
+  15000000 |       2 | 2011
+  30000000 |       3 | 2013
+  1350000 |       1 | 2018
+  10000000 |       4 | 2015
+*/
