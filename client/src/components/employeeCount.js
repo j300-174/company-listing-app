86bb809e08c57bc798bfbcadc17b5c348cecc244
j300-174 <jeddpicones@gmail.com>
2021-11-29 20:47:23 +0000

    moved h2 to main

--- client/src/components/employeeCount.js
+++ client/src/components/employeeCount.js
@@ -1,10 +1,8 @@
 export default function EmployeeCount({count}) {
-  let countVal = count;
   return (
     <div>
-      <h2>Statistics based on existing Company data</h2>
-      <p>Average employee count within existing companies</p>
-      <input type='text' placeholder={countVal} disabled/>
+      <h3>Average employee count within existing companies</h3>
+      <input type='text' placeholder={count} disabled />
     </div>
   );
 }
