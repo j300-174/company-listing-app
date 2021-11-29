adc697165d6c967e0ef0f75d22677c8e6662161a
j300-174 <jeddpicones@gmail.com>
2021-11-29 15:58:16 +0000

    truncated decimal and implemented as table

--- client/src/components/dealRaised.js
+++ client/src/components/dealRaised.js
@@ -1,18 +1,25 @@
 export default function DealRaised (dealObj) {
   let dealRaised = dealObj.deal;
   console.log(dealRaised);
-
-  // add currencies / table
+  dealRaised.map(dealObj => {
+    dealObj.avg = Math.trunc(dealObj.avg*100)/100;
+  });
 
   return (
     <div>
-      <h3>Average Deal raised for existing companies:</h3>
-      {dealRaised.map((company, index) => (
-        <div>
-          <li>{company.country}</li>
-          <li>{company.avg}</li>
-        </div>
-      ))}
+      <h3>Average Deal raised for existing companies by Country</h3>
+      <table>
+        <tr>
+          <th>Average</th>
+          <th>Country</th>
+        </tr>
+        {dealRaised.map((company, index) => (
+          <tr>
+            <td>{company.avg}</td>
+            <td>{company.country}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
