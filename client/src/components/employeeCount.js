687b633b38260a3fca600c2f320d86c88a52e5c0
j300-174 <jeddpicones@gmail.com>
2021-11-29 16:27:05 +0000

    displays value

--- client/src/components/employeeCount.js
+++ client/src/components/employeeCount.js
@@ -1,11 +1,10 @@
-export default function EmployeeCount (countObj) {
-  let count = countObj.count;
-  console.log(count);
+export default function EmployeeCount({count}) {
+  let countVal = count;
   return (
     <div>
       <h2>Statistics based on existing Company data</h2>
       <p>Average employee count within existing companies</p>
-      <p>{count}</p>
+      <input type='text' placeholder={countVal} disabled/>
     </div>
   );
 }
